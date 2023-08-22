71441e6566c8b4f5333cbec115e9ac1fef6b57a8
kihno <stistyle13@gmail.com>
2023-08-22 12:52:12 -0700

    Use graphql to fetch repository list

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,24 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { useState } from 'react';
+import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
   const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
 
-  const fetchRepositories = async () => {
-    setLoading(true);
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cach-and-network',
+    onCompleted: () => {
+      setRepositories(data.repositories);
+    },
+  });
 
-    const response = await fetch('http://10.0.2.15:5000/api/repositories');
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
+  return { repositories, loading, error };
 };
 
 export default useRepositories;
